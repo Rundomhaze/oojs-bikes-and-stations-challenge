7ca39f703e26d451f29ab143e3e07d942e3fb563
Alekskupr <leshakuprish@yandex.ru>
2020-01-28 14:20:43 +0300

    jasmine in console v3.0

--- src/bike.js
+++ src/bike.js
@@ -1,9 +1,5 @@
 class Bike {
-  constructor(id, color, modelYear) {
-    this.id = id;
-    this.color = color;
-    this.modelYear = modelYear;
-  }
+  
 }
 
 module.exports = { Bike };
--- src/station.js
+++ src/station.js
@@ -1,9 +1,5 @@
 class Station {
-  constructor(name, location, bikes) {
-    this.name = name;
-    this.location = location;
-    this.bikes = bikes;
-  }
+  
 }
 
 module.exports = { Station };
