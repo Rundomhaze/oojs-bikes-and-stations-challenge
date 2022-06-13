c59d2fb102e3bb4690691741561475340b545748
Alex <petenkov2822@gmail.com>
2022-06-13 19:14:46 +0300

    Release 0

--- src/station.js
+++ src/station.js
@@ -1,9 +1,30 @@
 class Station {
-  constructor(name, location, bikes) {
+  constructor(name, location, bikes = []) {
     this.name = name;
     this.location = location;
     this.bikes = bikes;
   }
+
+  newestBike() {
+    return this.bikes.sort((a,b) => b.modelYear - a.modelYear)[0]
+  };
+
+  empty() {
+    return this.bikes.length < 1 ? true : false
+  };
+
+  take(bike) {
+    return this.bikes.push(bike)
+  };
+
+  takeMultiple(bike) {
+   return (this.bikes = this.bikes.concat(bike))
+  }
+
+  release() {
+    return this.bikes.length < 1 ? undefined : this.newestBike() && this.bikes.shift()
+  }
+
 }
 
 module.exports = Station;
